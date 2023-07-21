105ebab7c6335f6509e8feb1ab55c6340208e1eb
jobala <japhethobalak@gmail.com>
2023-07-21 06:56:08 +0100

    an update

--- src/portals/portals-tooltip-ex/TooltipApp.js
+++ src/portals/portals-tooltip-ex/TooltipApp.js
@@ -2,22 +2,21 @@ import React from 'react';
 import Tooltip from './Tooltip';
 
 const TooltipApp = () => (
-    <div>
-        <h1>You guy am jenga-ing</h1>
-        <p>
-            Some text, some of which <Tooltip text="This is some more info about that 
+  <div>
+    <p>
+      Some text, some of which <Tooltip text="This is some more info about that 
             first thing you should find very interesting."> requires explanation</Tooltip>
-            (Scroll down for more)
-        </p>
+      (Scroll down for more)
+    </p>
 
-        <div style={{ lineHeight: 10 }}>Scroll down</div>
+    <div style={{ lineHeight: 10 }}>Scroll down</div>
 
-        <p>
-            <Tooltip text="You hovered over the second one!">Something else to hover over</Tooltip> is what this is.
-        </p>
+    <p>
+      <Tooltip text="You hovered over the second one!">Something else to hover over</Tooltip> is what this is.
+    </p>
 
-        <div style={{ lineHeight: 10 }}>Just more padding...</div>
-    </div>
+    <div style={{ lineHeight: 10 }}>Just more padding...</div>
+  </div>
 );
 
 export default TooltipApp;
